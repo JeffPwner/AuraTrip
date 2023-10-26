c7c85de793eceb20fb1ddb45e823aee985cd21c9
guicrispa <guicrispim1000@gmail.com>
2023-10-25 21:35:51 -0300

    teste

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,44 +16,22 @@
         <script src="/js/scripts.js"></script>
     </head>
     <body>
-        <header class="style_header">
-            <nav class="navbar navbar-expand-lg navbar-light">
-              <div class="collapse navbar-collapse" id="navbar">
-                <a href="/" class="navbar-brand">
-                  <img src="/img/auratrip-png.png" alt="Auratrip logo">
-                </a>
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a href="/dashboard" class="nav-link">Viagens</a>
-                  </li>    
-                  <li class="nav-item">
-                    <a href="/events/create" class="nav-link">Criar Viagem</a>
-                  </li>
-                  @auth
-                  <li class="nav-item">
-                    <form action="/logout" method="POST">
-                      @csrf
-                      <a href="/logout" 
-                        class="nav-link" 
-                        onclick="event.preventDefault();
-                        this.closest('form').submit();">
-                        Sair
-                      </a>
-                    </form>
-                  </li>
-                  @endauth
-                  @guest
-                  <li class="nav-item">
-                    <a href="/login" class="nav-link">Entrar</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="/register" class="nav-link">Cadastrar</a>
-                  </li>    
-                  @endguest
-                </ul>
-              </div>
-            </nav>
-          </header>
+      <a href="#home" class="skip-to-main-content-link">Saltar para o conteúdo principal</a>
+        <header id="main" class="style_header">
+          <div class="container_header">
+          <a href="index.html">
+              <img class="logo_img" src="https://res.cloudinary.com/dlsuopwkn/image/upload/v1697427878/samples/animals/auratripicon.png" width="30" height="30" alt="logo" />
+          </a>
+          <ul class="buttons" id="home" style="margin-bottom: 0;">
+              <li><a href="index.html">Viagens</a></li>
+              <li><a href="postar.html">Criar Viagem</a></li>
+              <li><a href="#">Cadastrar</a></li>
+              <li><a href="contato.html">Entrar</a></li>
+          </ul>
+          <!-- <div class="menucell"><ion-icon name="menu-outline"></ion-icon></div> -->
+          </div>
+          <h1 class="title_tromso" style="margin-top: 30px;">AuraTrip</h1>
+        </header>
         <main>
           <div class="container-fluid">
             <div class="row">
@@ -67,7 +45,6 @@
         <footer>
             <p>AuraTrip Company &copy; 2023</p>
         </footer>
-        
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
 </html>
